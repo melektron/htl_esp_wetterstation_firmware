6c5b7a52127f0ba374a22de56b61d84cc575e2ab
svejn <svejnoha.martin5@gmail.com>
2023-04-17 16:29:47 +0200

    changed port and sensor pin

--- include/config.hpp
+++ include/config.hpp
@@ -19,7 +19,7 @@ configuration definitions
 #define CONFIG_IO_LED_G D8
 
 // DHT pin
-#define CONFIG_IO_DHT D4
+#define CONFIG_IO_DHT D5
 
 // Display Type
 #define CONFIG_DISPLAY_TYPE U8G2_SH1106_128X64_NONAME_F_HW_I2C
